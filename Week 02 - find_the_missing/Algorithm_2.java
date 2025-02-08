6218452a50d0c0565b96e76d0a50164051aa6902
ShenalSen <shenal.unc555@gmail.com>
2025-02-08 13:07:28 +0530

    Implement findSmallestMissing method in Algorithm_2 to determine the smallest missing number in a sorted array

--- Week 02 - find_the_missing/Algorithm_2.java
+++ Week 02 - find_the_missing/Algorithm_2.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Algorithm_2 {
     
+    public static int findSmallestMissing(int[] arr) {
+        Arrays.sort(arr); // Time complexity: O(n log n)
+        
+        int missingNumber = 0;
+        for (int num : arr) {
+            if (num == missingNumber) {
+                missingNumber++;
+            } else if (num > missingNumber) {
+                break;
+            }
+        }
+        
+        return missingNumber;
+    }
     
     public static void main(String[] args) {
         
