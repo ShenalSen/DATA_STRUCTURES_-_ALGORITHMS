657e20081aac41e8f4692fd811fc5d55100e7fe9
ShenalSen <shenal.unc555@gmail.com>
2025-02-08 13:20:09 +0530

    Implement findSmallestMissing method in Algorithm_3 to determine the smallest missing number in a sorted array

--- Week 02 - find_the_missing/Algorithm_3.java
+++ Week 02 - find_the_missing/Algorithm_3.java
@@ -1,4 +1,17 @@
 public class Algorithm_3 {
+
+    public static int findSmallestMissing(int[] arr) {
+        int missingNumber = 0;
+        for (int num : arr) {
+            if (num == missingNumber) {
+                missingNumber++;
+            } else if (num > missingNumber) {
+                break;
+            }
+        }
+        return missingNumber;
+    }
+
     public static void main(String[] args) {
         
     }
