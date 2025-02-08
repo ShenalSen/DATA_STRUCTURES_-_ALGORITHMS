5f59748d33b7d9465d8e26cc025a1ed637194f4c
ShenalSen <shenal.unc555@gmail.com>
2025-02-08 13:08:25 +0530

    Add createInput method to Algorithm_2 for generating random input arrays

--- Week 02 - find_the_missing/Algorithm_2.java
+++ Week 02 - find_the_missing/Algorithm_2.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Algorithm_2 {
     
@@ -15,6 +16,15 @@ public class Algorithm_2 {
         }
         
         return missingNumber;
+    }
+
+    public static int[] createInput(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(size);
+        }
+        return arr;
     }
     
     public static void main(String[] args) {
