588230f87fcfabbffa6775296fc10517ad99e962
ShenalSen <shenal.unc555@gmail.com>
2025-02-08 13:00:50 +0530

    Add main method to Algorithm_1 for testing findSmallestMissing functionality

--- Week 02 - find_the_missing/Algorithm_1.java
+++ Week 02 - find_the_missing/Algorithm_1.java
@@ -16,5 +16,11 @@ public class Algorithm_1 {
         return missingNumber;
     }
 
+    
+    public static void main(String[] args) {
+        int[] input = {2, 7, 4, 0, 3, 2, 5, 1, 0, 3};
+        int result = findSmallestMissing(input);
+        System.out.println("The smallest missing number is: " + result);
+    }
 
 }
